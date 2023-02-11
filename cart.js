db3b872c2a6c64d2a45fd3e76fa84dbebc51c6d1
Water-Fleas <william.yin00@gmail.com>
2023-02-11 13:37:27 -0800

    cart.js work for eric

---
+++ cart.js
@@ -0,0 +1,24 @@
+function loadCart(){
+    let cartData = JSON.parse("cart-data.json");
+    return cartData;
+}
+
+function addCartItem(){
+
+}
+
+function removeCartItem(itemName){
+    let cart = loadCart();
+    let newCart = [];
+    for(let item in cart) {
+        if(item["name"] != itemName) {
+            newCart.push(item);
+        } 
+    }
+    //something to make new cart the new cart
+    displayCart();
+}
+
+function displayCart(){
+
+}
