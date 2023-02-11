9bdef11dca3e038b53f368282bda35e7cc9bbe0e
Yang Lu <xinyanglu664@gmail.com>
2023-02-11 17:01:43 -0500

    add untested scrape function

--- scrape.js
+++ scrape.js
@@ -1,1 +1,11 @@
-console.log("yo");
+async function scrape(url){
+    debugger
+    let response = await fetch(url)
+    response = await response.text()
+    const parser = new DOMParser
+    const htmlDoc = parser.parseFromString(response, "text/html")
+    console.log("running")
+    console.log(htmlDoc)
+}
+
+document.getElementById("button").onclick=scrape
