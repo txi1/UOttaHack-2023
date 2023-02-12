dbef29a1430bc16ec2b0e7a7616ffc0da3adc740
Water-Fleas <william.yin00@gmail.com>
2023-02-11 17:19:27 -0800

    removed some debugging lines
    Signed-off-by: Water-Fleas <william.yin00@gmail.com>

--- popup.js
+++ popup.js
@@ -2,10 +2,6 @@ import addItem from './addItem.js';
 import Cart from './cart.js';
 
 window.onload = function() {
-    console.log("dommy mommy");
-    // populateCartPage();
-
-    console.log(Cart.loadCart());
     // Cart.addCartItem("t1", "name1", "price", "image", "source");
     // Cart.addCartItem("t2", "name2", "price", "image", "source");
     // Cart.addCartItem("t2", "name3", "price", "image", "source");
